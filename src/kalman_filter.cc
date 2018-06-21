9410c1028b7be9b15b63b9fa0d58f262f0373985
Aaron Iglesias <aaron.iglesias@gmail.com>
2018-06-20 22:32:00 -0700

    Add kalman filter implementation.

---
+++ src/kalman_filter.cc
@@ -0,0 +1,36 @@
+#include "kalman_filter.h"
+#include "Eigen/Dense"
+
+std::unique_ptr<KalmanFilter>
+KalmanFilter::Create(const Eigen::VectorXd &x, const Eigen::MatrixXd &P,
+                     const Eigen::MatrixXd &F, const Eigen::MatrixXd &Q,
+                     const Eigen::MatrixXd &H, const Eigen::MatrixXd &R) {
+  // TODO(aaron-iglesias): Check if matrices have the correct dimensions.
+	return std::unique_ptr<KalmanFilter>(new KalmanFilter(x, P, F, Q, H, R));
+}
+
+KalmanFilter::KalmanFilter(const Eigen::VectorXd &x, const Eigen::MatrixXd &P,
+                           const Eigen::MatrixXd &F, const Eigen::MatrixXd &Q,
+                           const Eigen::MatrixXd &H, const Eigen::MatrixXd &R) {
+	x_ = x;
+	P_ = P;
+	F_ = F;
+	Q_ = Q;
+	H_ = H;
+	R_ = R;
+}
+
+void KalmanFilter::Predict() {
+  x_ = F_ * x_;
+  P_ = F_ * P_ * F_.transpose() + Q_;
+}
+
+void KalmanFilter::Update(const Eigen::VectorXd &z) {
+  const Eigen::VectorXd y = z - H_ * x_;
+  const Eigen::MatrixXd Ht = H_.transpose();
+  const Eigen::MatrixXd S = H_ * P_ * Ht + R_;
+  const Eigen::MatrixXd K = P_ * Ht * S.inverse();
+  x_ += K * y;
+  const Eigen::MatrixXd I = Eigen::MatrixXd::Identity(K.rows(), K.rows());
+  P_ = (I - K * H_) * P_;
+}
--- src/kalman_filter.h
+++ src/kalman_filter.h
@@ -1,5 +1,36 @@
+#ifndef KALMAN_FILTER_H
+#define KALMAN_FILTER_H
+
+#include "Eigen/Dense"
+
 class KalmanFilter {
 public:
+  static std::unique_ptr<KalmanFilter>
+  Create(const Eigen::VectorXd &x, const Eigen::MatrixXd &P,
+         const Eigen::MatrixXd &F, const Eigen::MatrixXd &Q,
+         const Eigen::MatrixXd &H, const Eigen::MatrixXd &R);
+
+  ~KalmanFilter();
+
+  void Predict();
+  void Update(const Eigen::VectorXd &z);
+
 private:
-	
+  KalmanFilter(const Eigen::VectorXd &x, const Eigen::MatrixXd &P,
+               const Eigen::MatrixXd &F, const Eigen::MatrixXd &Q,
+               const Eigen::MatrixXd &H, const Eigen::MatrixXd &R);
+  // Estimate.
+  Eigen::VectorXd x_;
+  // Uncertainty covariance.
+  Eigen::MatrixXd P_;
+  // State transition matrix.
+  Eigen::MatrixXd F_;
+  // Process covariance matrix.
+  Eigen::MatrixXd Q_;
+  // Measurement function.
+  Eigen::MatrixXd H_;
+  // Measurement noise.
+  Eigen::MatrixXd R_;
 };
+
+#endif
