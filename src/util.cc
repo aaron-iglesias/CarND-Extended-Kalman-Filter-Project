ac5e71cd2251a51f854742b79fe5a8157e519862
Aaron Iglesias <aaron.iglesias@gmail.com>
2018-06-21 19:16:13 -0700

    Add Jacobian calculation to util.

--- src/util.cc
+++ src/util.cc
@@ -20,8 +20,27 @@ CartesianToPolar(const Eigen::VectorXd &cartesian_coordinates) {
   return polar_coordinates;
 }
 
+// TODO(aaron-iglesias): Document expected ordering of contents for x_state
+// input.
 const Eigen::MatrixXd Jacobian(const Eigen::VectorXd &x_state) {
-  Eigen::MatrixXd jacobian;
-  return jacobian;
+  Eigen::MatrixXd Hj(3, 4);
+
+  const float p_x = x_state(0);
+  const float p_y = x_state(1);
+  const float v_x = x_state(2);
+  const float v_y = x_state(3);
+
+  const float min_denom = pow(10, -4);
+
+  const float range_squared = max(pow(p_x, 2) + pow(p_y, 2), min_denom);
+  const float range = max(sqrt(range_squared), min_denom);
+
+  Hj << p_x / range, p_y / range, 0, 0, -p_y / range_squared,
+      p_x / range_squared, 0, 0,
+      p_y * (v_x * p_y - v_y * p_x) / pow(range_squared, 1.5),
+      p_x * (v_y * p_x - v_x * p_y) / pow(range_squared, 1.5), p_x / range,
+      p_y / range;
+
+  return Hj;
 }
 } // namespace util
